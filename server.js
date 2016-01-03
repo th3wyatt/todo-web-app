dac9442a9671607a79fa58aac80ebfb87d5496bd
Jonathan Wyatt <jonathan.k.wyatt@gmail.com>
2016-01-03 11:36:18 -0800

    Added put to todos

--- server.js
+++ server.js
@@ -4,21 +4,8 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todoNextId = 4;
-
-var todos = [{
-		id: 1,
-		description: 'Meet mom for lunch',
-		completed: false,
-	}, {
-		id: 2,
-		description: 'go to market',
-		completed: false
-	}, {
-		id: 3,
-		description: 'win all the things',
-		completed: true
-	}]
+var todoNextId = 1;
+var todos = [];
 
 app.use(bodyParser.json());
 
@@ -64,6 +51,34 @@ app.delete('/todos/:id', function (req, res) {
 		todos = _.without(todos, matchedToDo);
 		res.json({ "message": "item deleted" });
 	}
+});
+
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedToDo = _.findWhere(todos, { id: todoId });
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+	
+	if (!matchedToDo) {
+		return res.status(400).send();
+	}
+	
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.descripton.trim() > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+	
+	_.extend(matchedToDo, validAttributes);
+
+	res.json(matchedToDo);
+
 });
 
 //app.use(middleware.logger);
