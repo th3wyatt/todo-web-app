1ca4193bd5d4b2307aa8d79ac087b66fa4860be3
Jonathan Wyatt <jonathan.k.wyatt@gmail.com>
2016-01-03 16:17:53 -0800

    Add completed filter

--- server.js
+++ server.js
@@ -9,14 +9,21 @@ var todos = [];
 
 app.use(bodyParser.json());
 
-//app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+	
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'true') {
+		filteredTodos = _.where(filteredTodos, { completed: true });
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
+		filteredTodos = _.where(filteredTodos, { completed: false });
+	}
+	res.json(filteredTodos);
+	
 });
 
 app.get('/todos/:id', function (req, res) {
@@ -80,13 +87,6 @@ app.put('/todos/:id', function (req, res) {
 	res.json(matchedToDo);
 
 });
-
-//app.use(middleware.logger);
-//app.use(middleware.requireAuthentication);
-
-//app.get('/about', middleware.requireAuthentication, function (req, res) {
-//	res.send('About us!!!');
-//});
 
 app.listen(PORT, function () {
 	console.log('express server started on port: ' + PORT);
