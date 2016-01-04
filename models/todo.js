c77eecc966c14c6685b41ce8aeb642dee02a89db
Jonathan Wyatt <jonathan.k.wyatt@gmail.com>
2016-01-03 23:20:54 -0800

    Add todo model and POST /todos

---
+++ models/todo.js
@@ -0,0 +1,17 @@
+﻿module.exports = function (sequelize, DataTypes) {
+	return sequelize.define('todo', {
+	description: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		validate: {
+			len: [ 1, 250 ]
+		}
+	},
+	completed: {
+		type: DataTypes.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	}
+
+});
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var middleware = require('./middleware.js');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todoNextId = 1;
@@ -44,18 +45,24 @@ app.get('/todos/:id', function (req, res) {
 	}
 });
 
-app.post('/todos', function (req, res){
+app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 	
-	if (!_.isBoolean(body.completed)|| !_.isString(body.description)|| body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function (todo) {
+		res.json(todo.toJSON());
+	}, function (e) {
+		res.status(400).json(e);
+	});
+
+	//if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	//	return res.status(400).send();
+	//}
 	
-	body.description = body.description.trim();
-	body.id = todoNextId++;
-	todos.push(body);
-	res.json(body);
-})
+	//body.description = body.description.trim();
+	//body.id = todoNextId++;
+	//todos.push(body);
+	//res.json(body);
+});
 
 app.delete('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
@@ -96,6 +103,9 @@ app.put('/todos/:id', function (req, res) {
 
 });
 
-app.listen(PORT, function () {
-	console.log('express server started on port: ' + PORT);
+db.sequelize.sync().then(function () {
+	app.listen(PORT, function () {
+		console.log('express server started on port: ' + PORT);
+	});
 });
+
