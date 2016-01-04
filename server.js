99fe548b5a73cccc435b2b9cb7fc7f356cd64068
Jonathan Wyatt <jonathan.k.wyatt@gmail.com>
2016-01-03 23:50:20 -0800

    Convert GET /todos/id to use sql

--- server.js
+++ server.js
@@ -29,21 +29,24 @@ app.get('/todos', function (req, res) {
 			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
 		});
 	}
-	
-	
 	res.json(filteredTodos);
-	
 });
 
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedToDo = _.findWhere(todos, { id: todoId });
-	if (matchedToDo) {
-		res.json(matchedToDo);
-	} else {
-		res.status(404).send();
-	}
+	
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
+		
 });
+
 
 app.post('/todos', function (req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
@@ -53,15 +56,6 @@ app.post('/todos', function (req, res) {
 	}, function (e) {
 		res.status(400).json(e);
 	});
-
-	//if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	//	return res.status(400).send();
-	//}
-	
-	//body.description = body.description.trim();
-	//body.id = todoNextId++;
-	//todos.push(body);
-	//res.json(body);
 });
 
 app.delete('/todos/:id', function (req, res) {
